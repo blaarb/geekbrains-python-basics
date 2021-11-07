56b4b78e4451bf0b9364dd24a86d18bec07da525
Tair Yunussov <ndir_90@mail.ru>
2021-11-08 00:13:45 +0600

    uncommented file generation section

--- Lesson5/task5.py
+++ Lesson5/task5.py
@@ -2,14 +2,15 @@
 # Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 
 # Генерация
-# import random
-#
-# with open("task5.txt", "w") as task5_txt:
-#     task5_txt.write(' '.join([str(random.randint(1, 101)) for x in range(1, 11)]))
+import random
+
+with open("task5.txt", "w") as task5_txt:
+    task5_txt.write(' '.join([str(random.randint(1, 101)) for x in range(1, 11)]))
 
 total = 0
 task5_input = open("task5.txt")
 for element in task5_input.readline().split():
     total += int(element)
+task5_input.close()
 
 print(f"Сумма чисел в файле: {total}")
