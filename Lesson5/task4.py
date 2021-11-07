36635083318777239ad402d04d9b3fb433d585e3
Tair Yunussov <ndir_90@mail.ru>
2021-11-07 19:23:46 +0600

    improved task 4 code

--- Lesson5/task4.py
+++ Lesson5/task4.py
@@ -1,7 +1,6 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
-import os
 
 numbers_eng2ru = {
     "One": "Один",
@@ -11,12 +10,6 @@ numbers_eng2ru = {
     "Five": "Пять"
 }
 
-
-def get_russian_word(input_string):
-    return numbers_eng2ru.get(input_string)
-
-
-# os.remove("task4_output.txt")
 file_to_erase = open("task4_output.txt", "w")
 file_to_erase.close()
 
@@ -27,4 +20,4 @@ with open("task4.txt") as source_file:
         numeral_number = line.split()[2]
         with open("task4_output.txt", "a") as output_file:
             print(source_file.encoding)
-            output_file.write(f"{get_russian_word(extracted_word)} - {numeral_number}\n")
+            output_file.write(f"{numbers_eng2ru.get(extracted_word)} - {numeral_number}\n")
