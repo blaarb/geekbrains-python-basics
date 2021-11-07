7c33c696f5bad8d50d893bf7f1796fda427347fc
Tair Yunussov <ndir_90@mail.ru>
2021-11-07 22:26:31 +0600

    added task 5

---
+++ Lesson5/task5.py
@@ -0,0 +1,15 @@
+# Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
+# Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
+
+# Генерация
+# import random
+#
+# with open("task5.txt", "w") as task5_txt:
+#     task5_txt.write(' '.join([str(random.randint(1, 101)) for x in range(1, 11)]))
+
+total = 0
+task5_input = open("task5.txt")
+for element in task5_input.readline().split():
+    total += int(element)
+
+print(f"Сумма чисел в файле: {total}")
