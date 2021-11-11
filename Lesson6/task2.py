c2088f50bfd96dad450223b21e7493e835d18b99
Tair Yunussov <ndir_90@mail.ru>
2021-11-11 19:47:04 +0600

    changed task 2 according to task description

--- Lesson6/task2.py
+++ Lesson6/task2.py
@@ -1,12 +1,14 @@
 
 
 class Road:
-    _length = 20
-    _width = 5000
+    _length = 0
+    _width = 0
 
     def calculate_volume(self, cm_mass, thickness):
         return self._length * self._width * cm_mass * thickness
 
 
 road01 = Road()
+road01._length = 5000
+road01._width = 20
 print(road01.calculate_volume(25, 5))
