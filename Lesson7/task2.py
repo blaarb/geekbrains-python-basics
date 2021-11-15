4429eb9f311f918d760ee334a49b25d0d95a4edd
Tair Yunussov <ndir_90@mail.ru>
2021-11-15 22:21:14 +0600

    added 2nd task

--- Lesson7/task2.py
+++ Lesson7/task2.py
@@ -1,16 +1,35 @@
-class Clothes:
-    pass
+from abc import ABC, abstractmethod
 
 
-class Coat(Clothes)
-    size = 0
-    size / 6.5 + 0.5
+class Clothes(ABC):
 
-class Costume(Clothes)
-    height = 0
-    2 * height + 0.3
+    @abstractmethod
+    def calc_volume(self):
+        pass
 
-    def
 
-        # https: // python - course.eu / oop / the - abc - of - abstract - base - classes.php
-        # https: // www.freecodecamp.org / news / python - property - decorator /
+class Coat(Clothes):
+
+    def __init__(self, size):
+        self.size = size
+
+    @property
+    def calc_volume(self):
+        return self.size / 6.5 + 0.5
+
+
+class Costume(Clothes):
+
+    def __init__(self, height):
+        self.height = height
+
+    @property
+    def calc_volume(self):
+        return 2 * self.height + 0.3
+
+
+costume1 = Costume(20)
+print(costume1.calc_volume)
+
+coat1 = Coat(20)
+print(coat1.size)
