7179f3b0a216437298f3d3901988e4d6db7b7756
Tair Yunussov <ndir_90@mail.ru>
2021-11-16 18:56:17 +0600

    added 1st task

--- Lesson7/task1.py
+++ Lesson7/task1.py
@@ -1,19 +1,30 @@
+import random
+
+
 class Matrix:
+
     def __init__(self, input_list):
-        self.input_list = input_list
+        self.list_of_lists = input_list
 
     def __str__(self):
         result_string = ''
-        for list_element in self.input_list:
+        for list_element in self.list_of_lists:
             result_string += str(f"{list_element}\n")
         return result_string
 
     def __add__(self, other):
-        for i in range(len(self.input_list))
+        result_matrix = []
+        for i in range(len(self.list_of_lists)):
+            result_row = []
+            for j in range(len(self.list_of_lists[i])):
+                result_row.append(self.list_of_lists[i][j] + other.list_of_lists[i][j])
+            result_matrix.append(result_row)
+        return result_matrix
 
-m1 = Matrix([[1, 2, 3], [4, 5, 6]])
-m2 = Matrix([[7, 8, 9], [1, 2, 3]])
+
+m1 = Matrix([random.sample(range(1, 10), 3), random.sample(range(1, 10), 3), random.sample(range(1, 10), 3)])
+m2 = Matrix([random.sample(range(1, 10), 3), random.sample(range(1, 10), 3), random.sample(range(1, 10), 3)])
 
 print(m1)
-
-print([4, 5, 6] + [4, 5, 6])
+print(m2)
+print(m1 + m2)
